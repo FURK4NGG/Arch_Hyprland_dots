407b8602406988750d837d8692d31aa57de71dfb
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2025-09-02 14:33:10 +0300

    Delete .config/scripts directory

--- .config/scripts/screenrec.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-notify-send "Ekran kaydı basladı" && wf-recorder -f ~/Resimler/screen_capture_$(date +%Y%m%d-%H%M%S).mp4 -a alsa_output.pci-0000_0d_00.6.analog-stereo.monitor
--- .config/scripts/screenrecstop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pkill wf-recorder && notify-send "Ekran kaydı alındı"
