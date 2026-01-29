1be7c367580d7f05a6aed8834784632b1beb8c9b
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2026-01-29 14:14:34 +0300

    Create gamemode.sh

---
+++ .config/scripts/gamemode.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+STATE_FILE="$HOME/.cache/toggle-gamemode-state"
+
+if [ -f "$STATE_FILE" ]; then
+    # İkinci basış (state varsa)
+    rm "$STATE_FILE"
+    hyprctl keyword decoration:blur:enabled true
+    hyprctl keyword animations:enabled true
+    hyprctl keyword general:allow_tearing false
+    hyprctl keyword decoration:drop_shadow false
+    notify-send "Game Mode" "game mode is deactive"
+    hyprctl dispatch submap reset
+else
+    touch "$STATE_FILE"
+    hyprctl keyword decoration:blur:enabled false
+    hyprctl keyword animations:enabled false
+    hyprctl keyword general:allow_tearing true
+    hyprctl keyword decoration:drop_shadow false
+    notify-send "Game Mode" "game mode is active"
+    hyprctl dispatch submap gamemode
+fi
