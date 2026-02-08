f506c9974158468d7da5cfe15cd4b3a5d7c4932e
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2026-02-08 21:24:58 +0300

    Update screenprint.sh

--- .config/scripts/screenprint.sh
+++ .config/scripts/screenprint.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-bash -c 'grim "$HOME/Resimler/tam-ekran-$(date +%Y%m%d-%H%M%S).png" && notify-send "Ekran görüntüsü alındı"'
+
+DIR="$HOME/Resimler"
+FILE="tam-ekran-$(date +%Y%m%d-%H%M%S).png"
+ARG="$1"
+
+
+if [ -z "$ARG" ]; then
+    grim "$DIR/$FILE" \
+    && notify-send "Screenshot taken" "All Screens"
+    exit 0
+fi
+
+case "$ARG" in
+    only-one)
+        OUTPUT=$(slurp -o -f "%o") || exit 0
+        grim -o "$OUTPUT" "$DIR/$FILE" \
+        && notify-send "Screenshot taken" "Selected Screen"
+        ;;
+    -h|help|-help|--help|*)
+        echo "Kullanim/Usage:"
+        echo "  $0           ->  Screenshot for all screens in one save"
+        echo "  $0 only-one  ->  Screenshot for selected screen"
+        exit 0
+        ;;
+esac
