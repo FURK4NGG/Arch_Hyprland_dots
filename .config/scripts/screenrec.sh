a569ffef7fa9c2bdbc681201f987b20dd200e6f3
FURK4NGG <105324908+FURK4NGG@users.noreply.github.com>
2026-02-08 21:52:06 +0300

    Update screenrec.sh

--- .config/scripts/screenrec.sh
+++ .config/scripts/screenrec.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-STATE_FILE="$HOME/.cache/toggle-example.state"
+STATE_FILE="$HOME/.cache/screenrec.state"
+DIR="$HOME/Resimler"
+FILE="$DIR/screen_capture_$(date +%Y%m%d-%H%M%S).mp4"
+
+# Help
+if [ $# -gt 0 ]; then
+    echo "Usage:"
+    echo "  $0   ->  Toggle screen recording"
+    exit 0
+fi
 
 if [ -f "$STATE_FILE" ]; then
-    # İkinci basış (state varsa)
-    rm "$STATE_FILE"
-    pkill wf-recorder && notify-send "Ekran kaydı alındı"
-    # İkinci basışta çalışacak komut
-    # echo "İkinci komut çalıştı"
+    rm -f "$STATE_FILE"
+    pkill wf-recorder
+    notify-send "Recording stopped"
 else
-    # İlk basış (state yoksa)
+    # Tek ekran seçimi (interaktif ama güvenli)
+    GEOM=$(slurp -o -f "%x,%y %wx%h" 2>/dev/null) || exit 0
+
     touch "$STATE_FILE"
-    notify-send "Ekran kaydı basladı" && wf-recorder -f ~/Resimler/screen_capture_$(date +%Y%m%d-%H%M%S).mp4 -a alsa_output.pci-0000_0d_00.6.analog-stereo.monitor
-    # İlk basışta çalışacak komut
-    # echo "İlk komut çalıştı"
+    notify-send "Recording started"
+
+    wf-recorder \
+        -g "$GEOM" \
+        -f "$FILE" \
+        -a alsa_output.pci-0000_0d_00.6.analog-stereo.monitor &
 fi
